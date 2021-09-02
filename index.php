9b27308567e7cb450ea5a80b356f22d020cad2c0
Sushant Sigdel <65490823+Su5hant@users.noreply.github.com>
2021-09-02 11:19:18 +0545

    Update index.php

--- index.php
+++ index.php
@@ -200,6 +200,25 @@
                 </div>
                 <div class="column right">
                     <div class="text">Message me</div>
+                <!-- Form -->
+                    <?php 
+if(isset($_POST['submit'])){
+    $to = "sushantsigdel.com.np@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $subject2 = "Copy of your form submission";
+    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
                     <form action="#" method="Post">
                         <div class="fields">
                             <div class="field name">
@@ -228,7 +247,7 @@
         <span>Created By Sushant Sigdel</a> | <span class="far fa-copyright"></span> 2021 All rights reserved.</span>
     </footer>
     <script src="script.js"></script>
-     <?php include 'message.php'; ?>
+     
 </body>
 </html>
 
